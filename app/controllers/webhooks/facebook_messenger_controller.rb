b24bfdd87ed93c4e21e98dd1f8a431f2f04ce9d1
Girish <technopreneurG@users.noreply.github.com>
2016-05-17 11:16:19 +0530

    handle verify request & chat messages, send back simple text ack!

--- app/controllers/webhooks/facebook_messenger_controller.rb
+++ app/controllers/webhooks/facebook_messenger_controller.rb
@@ -1,9 +1,27 @@
 class Webhooks::FacebookMessengerController < ApplicationController
-  def verify
-    Rails.logger.info "Got verify request"
-  end
+    include Webhooks::FacebookMessengerHelper
 
-  def chat
-    Rails.logger.info "Got chat request"
-  end
+    def verify
+        Rails.logger.info "Got verify request"
+        if params['hub.verify_token'].present? and params['hub.verify_token']==ENV['fb_verify_token']
+            render plain: params['hub.challenge'];
+        else
+            render nothing: true
+        end
+    end
+
+    def chat
+        Rails.logger.info "Got chat request"
+        messaging_events = params['entry'][0]['messaging']
+        if messaging_events.present?
+            messaging_events.each do |e|
+                if e['message'].present?
+                    sender_id = e['sender']['id']
+                    text = e['message']['text']
+                    send_text_message(sender_id, "got : #{text}")
+                end
+            end
+        end
+        render nothing: true
+    end
 end
--- app/helpers/webhooks/facebook_messenger_helper.rb
+++ app/helpers/webhooks/facebook_messenger_helper.rb
@@ -1,2 +1,25 @@
 module Webhooks::FacebookMessengerHelper
+    def send_text_message(to, text)
+        begin
+            body={
+                recipient: {id: to},
+                message: {text: text}
+            }
+            url='https://graph.facebook.com/v2.6/me/messages?access_token='+ENV['fb_access_token']
+
+            uri = URI.parse(url)
+            http = Net::HTTP.new(uri.host, uri.port)
+            http.use_ssl = true
+            http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+            request = Net::HTTP::Post.new(uri.request_uri)
+            request.content_type = 'application/json'
+            request.body = body.to_json
+
+            response = http.request(request)
+            Rails.logger.info response.body
+        rescue => e #todo: instead handle error response codes
+            puts "Error: #{e.message}"
+        end
+    end
 end
